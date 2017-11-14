14f64e829fa6f231ae82d75d448ef71c0eac5afb
Aioramu <31703899+Aioramu@users.noreply.github.com>
2017-11-14 19:43:01 +0300

    Update main2.c

--- main2.c
+++ main2.c
@@ -1,48 +1,47 @@
-#include "stdio.h" 
-#include "stdlib.h" 
+#include "stdio.h"
+#include "stdlib.h"
 
-main(int arg, char *argv[]) 
-{ 
-float gr; 
-char c = 0; 
-gr=atof(argv[1]); 
-if(arg==3) 
-c = *argv[2]; 
-if(c=='C' || !c || c=='c') 
-{ 
-if(gr<-273) 
-printf("%.2f C:\nBelow Absolut zero!!\Bn", gr); 
-else 
-{ 
-printf("C: %.2f \n", gr); 
-printf("K: %.2f \n", gr + 273.15); 
-printf("F: %.2f \n", gr*9/5.0+32); 
-printf("\n"); 
-} 
-} 
-if(c=='F' || !c || c=='f') 
-{ 
-if(gr<-459) 
-printf("%.2f C:\nBelow Absolut zero!!\n", gr); 
-else 
-{ 
-printf("C: %.2f \n", (gr-32)*5/9.0); 
-printf("K: %.2f \n", (gr-32)*5/9.0 + 273.15); 
-printf("F: %.2f \n", gr); 
-printf("\n"); 
-} 
-} 
-if(c=='K' || !c || c=='k') 
-{ 
-if(gr<0) 
-printf("%.2f C:\nBelow Absolut zero!!!\n", gr); 
-else 
-{ 
-printf("C: %.2f \n", gr-273.15); 
-printf("K: %.2f \n", gr); 
-printf("F: %.2f \n", (gr-273.15)*9/5.0+32); 
-printf("\n"); 
-} 
-} 
-return 0; 
-}
+main(int argc, char *argv[])
+{
+    float gr;
+    char c = 0;
+    gr = atof(argv[1]);
+    if(argc==3)
+        c = *argv[2];
+    if(c=='C' || !c || c=='c')
+    {
+        if(gr<-273)
+            printf("%.2f C:\nBelow absolute zero\n", gr);
+        else
+        {
+            printf("C: %.2f \n", gr);
+            printf("K: %.2f \n", gr + 273.15);
+            printf("F: %.2f \n", gr*9/5.0+32);
+            printf("\n");
+        }
+    }
+    if(c=='F' || !c || c=='f')
+    {
+       if(gr<-459)
+          printf("%.2f C:\nBelow absolute zero\n", gr);
+       else
+       {
+          printf("F: %.2f \n", gr);
+          printf("C: %.2f \n", (gr-32)*5/9.0);
+          printf("K: %.2f \n", (gr-32)*5/9.0 + 273.15);
+          printf("\n");
+       }
+    }
+    if(c=='K' || !c || c=='k')
+    {
+      if(gr<0)
+         printf("%.2f C:\nBelow absolute zero\n", gr);
+      else
+      {
+         printf("K: %.2f \n", gr);
+         printf("C: %.2f \n", gr-273.15);
+         printf("F: %.2f \n", (gr-273.15)*9/5.0+32);
+         printf("\n");
+      }
+    }
+return 0;
